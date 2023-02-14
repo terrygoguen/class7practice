216b8cf028786c83fbd2ceb09cf316e73a6dd18e
terrygoguen <terry.goguen@kellogg.northwestern.edu>
2023-02-14 21:58:23 +0000

    done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,18 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+    @posts = Post.new
+  end 
+
+  def create
+   @posts = Post.new 
+   @posts["author"] = params["post"]["author"]
+   @posts["body"] = params["post"]["body"]
+   @posts["image"] = params["post"]["image"]
+   @posts.save
+   redirect_to "/posts"
+  end 
+
 end
+ 
