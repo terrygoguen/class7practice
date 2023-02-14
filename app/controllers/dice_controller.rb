5f74c50d18d4ef076e68224a2be83457adcc2610
terrygoguen <terry.goguen@kellogg.northwestern.edu>
2023-02-14 21:37:11 +0000

    phase1

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,3 +1,12 @@
 class DiceController < ApplicationController
 
+    def index 
+        #die1 = "something"
+        @die1 = rand(1..6)
+        @die2 = rand(1..6)
+        @total = @die1 + @die2
+        @username = params["username"]
+        # render the file in /views/dice/index/html.erb 
+    end 
+
 end
